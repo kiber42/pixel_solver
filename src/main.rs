44ac4be5e56fe0a06d07fd2efa709687608adf93
Kilian Rosbach <kilian.rosbach@posteo.de>
2019-11-11 17:30:24 +0100

    Refactored into several structs; showing sequence

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
+#[derive(Clone, Copy)]
 struct Puzzle {
     sheet: [[u8; GRID]; GRID],
     col_counts: [u8; GRID],
     row_counts: [u8; GRID]
 }
 
-const GRID : usize = 15;
-const PUZZLE : Puzzle = Puzzle {
-    sheet : [
+const GRID: usize = 15;
+const PUZZLE: Puzzle = Puzzle {
+    sheet: [
         [ 0, 1, 2, 2, 3, 3, 3, 3, 3, 4, 4, 5, 5, 6, 7],
         [ 0, 1, 2, 8, 8, 3, 9,10,11,12,12,13,14,15,16],
         [17,18,18,19,19, 9, 9, 9,20,21,22,13,14,16,16],
@@ -22,14 +23,14 @@ const PUZZLE : Puzzle = Puzzle {
         [78,79,79,80,80,81,86,86,82,82,83,84,85,85,87],
         [78,78,79,88,89,86,86,86,90,82,90,91,91,92,87],
         [78,78,79,93,93,93,94,94,90,90,90,91,91,91,87]],
-    col_counts : [8, 6, 10, 5, 4, 4, 7, 4, 7, 6, 6, 5, 10, 7, 2],
-    row_counts : [5, 11, 12, 3, 9, 8, 4, 8, 6, 3, 2, 2, 5, 6, 7]
+    col_counts: [8, 6, 10, 5, 4, 4, 7, 4, 7, 6, 6, 5, 10, 7, 2],
+    row_counts: [5, 11, 12, 3, 9, 8, 4, 8, 6, 3, 2, 2, 5, 6, 7]
 };
 
 /*
-const GRID : usize = 10;
-const PUZZLE : Puzzle = Puzzle {
-    sheet : [
+const GRID: usize = 10;
+const PUZZLE: Puzzle = Puzzle {
+    sheet: [
         [ 0, 1, 1, 2, 2, 3, 3, 4, 4, 5],
         [ 0, 0, 1, 6, 7, 7, 8, 9,10, 5],
         [11,11,12,13,13,14,15,15,16,17],
@@ -46,18 +47,60 @@ const PUZZLE : Puzzle = Puzzle {
 */
 
 /*
-const GRID : usize = 5;
-const PUZZLE : Puzzle = Puzzle {
-    sheet : [
+const GRID: usize = 5;
+const PUZZLE: Puzzle = Puzzle {
+    sheet: [
         [ 0, 1, 2, 2, 3],
         [ 0, 1, 2, 4, 4],
         [ 5, 6, 6, 7, 7],
         [ 8, 9, 9,10,10],
         [ 8, 9,11,11,11]],
-    col_counts : [2, 3, 2, 1, 2],
-    row_counts : [1, 2, 2, 3, 2]
+    col_counts: [2, 3, 2, 1, 2],
+    row_counts: [1, 2, 2, 3, 2]
 };
 */
+
+#[derive(PartialEq)]
+#[derive(Copy, Clone)]
+#[derive(Debug)]
+enum CellState
+{
+    Filled,
+    Empty,
+    Undecided
+}
+type Assignment = Vec<CellState>;
+
+type Sequence = Vec<u8>;
+
+#[derive(Clone)]
+struct Solution
+{
+    assignment: Assignment,
+    sequence: Sequence,
+}
+
+// holds pre-computed structures used throughout the solving process
+use std::collections::HashMap;
+struct Solver
+{
+    puzzle: Puzzle,
+    cell_sizes_in_rows: [HashMap<u8, u8>; GRID],
+    cell_sizes_in_cols: [HashMap<u8, u8>; GRID],
+}
+
+fn main() {
+    let solver = Solver::new(&PUZZLE);
+    if let Some(solution) = solver.run()
+    {
+        for step in &solution.sequence
+        {
+            println!("Mark {} as {:?}", step, solution.assignment[*step as usize]);
+        }
+        println!("Puzzle solved.");
+    }
+    else { println!("Puzzle is impossible."); }
+}
 
 #[derive(PartialEq)]
 enum GameState
@@ -68,162 +111,14 @@ enum GameState
 }
 
 #[derive(PartialEq)]
-#[derive(Copy, Clone)]
-enum CellState
-{
-    Filled,
-    Empty,
-    Undecided
-}
-type Assignment = Vec<CellState>;
-
-#[derive(PartialEq)]
 enum Target
 {
     Row(usize),
     Col(usize)
 }
 
-use std::fmt;
-impl fmt::Display for Target {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match &self
-        {
-            Target::Row(r) => write!(f, "row {}", r + 1),
-            Target::Col(c) => write!(f, "column {}", c + 1),
-        }
-    }
-}
-
-fn main() {
-    if PUZZLE.run_solver() { println!("Puzzle solved."); }
-    else { println!("Puzzle is impossible."); }
-}
-
 impl Puzzle
 {
-    fn run_solver(&self) -> bool
-    {
-        if !self.validate()
-        {
-            panic!("Puzzle definition is invalid.");
-        }
-    
-        let max_cell_per_row = self.sheet.iter().map(|row| row.iter().max().unwrap());
-        let max_cell = *max_cell_per_row.max().unwrap() as usize;
-        let assignment = vec![CellState::Undecided; max_cell + 1];
-        let mut sequence : Vec<(u8,CellState)> = Vec::new();
-        self.solve(assignment, &mut sequence)
-
-        // TODO: Print path to solution
-    }
-
-    fn solve(&self, mut assignment: Assignment, sequence: &mut Vec<(u8,CellState)>) -> bool
-    {
-        self.improve(&mut assignment);
-        println!("Deduced:");
-        self.print(&assignment, true);
-
-        match self.get_state(&assignment)
-        {
-            GameState::Solved => return true,
-            GameState::Impossible => return false,
-            _ => {}
-        }
-
-        if let Some((index,_)) = assignment.iter().enumerate().find(|(_,&x)| x == CellState::Undecided)
-        {
-            println!("Setting {} to FILLED (guess)", index);
-            self.print(&assignment, true); // for debugging
-
-            assignment[index] = CellState::Filled;
-            if self.solve(assignment.to_vec(), sequence)
-            {
-                sequence.push((index as u8, CellState::Filled));
-                return true;
-            }
-
-            println!("Setting {} to EMPTY (alternative was ruled out)", index);
-            assignment[index] = CellState::Empty;
-            if self.solve(assignment, sequence)
-            {
-                sequence.push((index as u8, CellState::Empty));
-                return true;
-            }
-            return false;
-        }
-        panic!("Should not get here.");
-    }
-
-    fn improve(&self, assignment: &mut Assignment)
-    {
-        loop
-        {
-            // TODO: Instead of iterating over all rows and columns like this,
-            // keep a list of rows and cols that were recently changed.
-            let mut shall_continue = false;
-            for row_or_col in 0..GRID
-            {
-                if self.get_state(assignment) == GameState::Impossible { return; }                
-                if self.improve_once(assignment, Target::Row(row_or_col)) { shall_continue = true; }
-                if self.improve_once(assignment, Target::Col(row_or_col)) { shall_continue = true; }
-            }
-            if !shall_continue { break; }
-        }
-    }
-
-    fn improve_once(&self, assignment: &mut Assignment, row_or_col : Target) -> bool
-    {
-        // determine which cells are in this row or column and how many squares they cover
-        use std::collections::HashMap;
-        let mut cell_sizes_in_target : HashMap<u8, u8> = HashMap::new();
-        for other in 0..GRID
-        {
-            let cell = match row_or_col
-            {
-                Target::Row(r) => self.sheet[r][other],
-                Target::Col(c) => self.sheet[other][c]
-            };
-            *cell_sizes_in_target.entry(cell).or_insert(0) += 1;
-        }
-
-        let (min, max) = self.get_range(assignment, &row_or_col);
-        let current = match row_or_col
-        {
-            Target::Row(r) => self.row_counts[r],
-            Target::Col(c) => self.col_counts[c]
-        };
-        let mut to_fill = current - min;
-        let mut to_clear = max - current;
-
-        let mut shall_continue = false;
-        loop 
-        {
-            let mut changed = false;
-            for (&index, &count) in cell_sizes_in_target.iter()
-            {
-                let index = index as usize;
-                if assignment[index] != CellState::Undecided { continue; }
-                if count > to_fill && count > to_clear { return false; }
-                if count > to_clear
-                {
-                    assignment[index] = CellState::Filled;
-                    changed = true;
-                    to_fill -= count;
-                }
-                else if count > to_fill
-                {
-                    assignment[index] = CellState::Empty;
-                    changed = true;
-                    to_clear -= count;
-                }
-            }
-            if !changed { break; }
-            shall_continue = true;
-        }
-        shall_continue
-    }
-
     fn validate(&self) -> bool
     {
         let col_counts = self.col_counts.iter();
@@ -234,7 +129,150 @@ impl Puzzle
         self.col_counts.iter().max().unwrap() <= &(GRID as u8) &&
         self.row_counts.iter().max().unwrap() <= &(GRID as u8)
     }
+}
 
+impl Solution
+{
+    fn new(puzzle: &Puzzle) -> Solution
+    {
+        let max_cell_per_row = puzzle.sheet.iter().map(|row| row.iter().max().unwrap());
+        let max_cell = *max_cell_per_row.max().unwrap() as usize;
+        Solution {
+            assignment: vec![CellState::Undecided; max_cell + 1],
+            sequence: Vec::new()
+        }
+    }
+}
+
+impl Solver
+{
+    fn new(puzzle: &Puzzle) -> Solver
+    {
+        if !puzzle.validate()
+        {
+            panic!("Puzzle definition is invalid.");
+        }
+
+        // determine which cells are in this row or column and how many squares they cover
+        let mut cell_sizes_in_rows : [HashMap<u8, u8>; GRID] = Default::default();
+        let mut cell_sizes_in_cols : [HashMap<u8, u8>; GRID] = Default::default();
+        for row in 0..GRID
+        {
+            for col in 0..GRID
+            {
+                let cell = puzzle.sheet[row][col];
+                *cell_sizes_in_rows[row].entry(cell).or_insert(0) += 1;
+                *cell_sizes_in_cols[col].entry(cell).or_insert(0) += 1;
+            }
+        }
+
+        Solver
+        {
+            puzzle: *puzzle,
+            cell_sizes_in_rows: cell_sizes_in_rows,
+            cell_sizes_in_cols: cell_sizes_in_cols,
+        }
+    }
+
+    fn run(&self) -> Option<Solution>
+    {
+        self.recurse(&Solution::new(&self.puzzle))
+    }
+
+    fn recurse(&self, partial: &Solution) -> Option<Solution>
+    {
+        let mut updated = partial.clone();
+        self.improve(&mut updated);
+        println!("Deduced:");
+        self.puzzle.print(&updated.assignment, true);
+
+        match self.puzzle.get_state(&updated.assignment)
+        {
+            GameState::Solved => return Some(updated),
+            GameState::Impossible => return None,
+            _ => {}
+        }
+
+        if let Some((index,_)) = updated.assignment.iter().enumerate().find(|(_,&x)| x == CellState::Undecided)
+        {
+            updated.sequence.push(index as u8);
+            for state in &[CellState::Filled, CellState::Empty]
+            {
+                println!("Assigning {} as {:?}", index, state);
+                updated.assignment[index] = *state;
+                let result = self.recurse(&updated);
+                if result.is_some() { return result; }
+            }
+            return None;
+        }
+        panic!("Should not get here.");
+    }
+
+    fn improve(&self, partial: &mut Solution)
+    {
+        loop
+        {
+            // TODO: Instead of iterating over all rows and columns like this,
+            // keep a list of rows and cols that were recently changed.
+            let mut changed = false;
+            for row_or_col in 0..GRID
+            {
+                changed |= self.improve_once(partial, Target::Row(row_or_col));
+                changed |= self.improve_once(partial, Target::Col(row_or_col));
+            }
+            if !changed { break; }
+        }
+    }
+
+    fn improve_once(&self, partial: &mut Solution, row_or_col: Target) -> bool
+    {
+        let (min, max) = self.puzzle.get_range(&partial.assignment, &row_or_col);
+        let goal = match row_or_col
+        {
+            Target::Row(r) => self.puzzle.row_counts[r],
+            Target::Col(c) => self.puzzle.col_counts[c]
+        };
+        if goal < min || goal > max { return false; }
+        let mut to_fill = goal - min;
+        let mut to_clear = max - goal;
+
+        let mut changed = false;
+        let cell_sizes_in_target = match row_or_col
+        {
+            Target::Row(r) => &self.cell_sizes_in_rows[r],
+            Target::Col(c) => &self.cell_sizes_in_cols[c]
+        };
+        loop
+        {
+            let mut seq = Vec::new();
+            for (&index, &count) in cell_sizes_in_target
+            {
+                let ref mut assign = partial.assignment[index as usize];
+                if *assign != CellState::Undecided { continue; }
+                if count > to_fill && count > to_clear { return false; }
+                if count > to_clear
+                {
+                    *assign = CellState::Filled;
+                    seq.push(index);
+                    to_fill -= count;
+                }
+                else if count > to_fill
+                {
+                    *assign = CellState::Empty;
+                    seq.push(index);
+                    to_clear -= count;
+                }
+            }
+            if seq.is_empty() { break; }
+            changed = true;
+            partial.sequence.extend(seq);
+        }
+        changed
+    }
+}
+
+impl Puzzle
+{
     fn get_range(&self, assignment: &Assignment, row_or_col: &Target) -> (u8, u8)
     {
         let mut min = 0;
@@ -248,8 +286,8 @@ impl Puzzle
             };
             match assignment[cell as usize]
             {
-                CellState::Empty => { max -= 1; }
-                CellState::Filled => { min += 1; }
+                CellState::Empty => max -= 1,
+                CellState::Filled => min += 1,
                 CellState::Undecided => {}
             }
         }
@@ -265,12 +303,7 @@ impl Puzzle
                                     (Target::Col(index), self.col_counts[index])]
             {
                 let (min, max) = self.get_range(assignment, &target);
-                if min > *goal || max < *goal
-                {
-                    println!("Current state unsolvable: {}, target count {} is not in the possible range {} - {}",
-                        target, goal, min, max);
-                    return GameState::Impossible;
-                }
+                if min > *goal || max < *goal { return GameState::Impossible; }
                 if min != max { decided = false; }
             }
         }
