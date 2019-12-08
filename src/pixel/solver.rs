34d3c2b50eb86f7f25171e2352dce29c87260fee
Kilian Rosbach <kilian.rosbach@posteo.de>
2019-12-08 11:03:25 +0100

    Fewer calls to improve_once; significant overhead

--- src/pixel/solver.rs
+++ src/pixel/solver.rs
@@ -2,6 +2,7 @@ use super::puzzle::{Puzzle, GRID};
 use super::solution::{CellState, Solution};
 
 use std::collections::HashMap;
+use std::collections::HashSet;
 
 // holds pre-computed structures used throughout the solving process
 pub struct Solver
@@ -9,9 +10,10 @@ pub struct Solver
     puzzle: Puzzle,
     cell_sizes_in_rows: [HashMap<u8, u8>; GRID],
     cell_sizes_in_cols: [HashMap<u8, u8>; GRID],
+    affected_targets_by_cell: Vec<Vec<Target>>,
 }
 
-#[derive(PartialEq)]
+#[derive(PartialEq, Eq, Hash, Clone)]
 pub enum Target
 {
     Row(usize),
@@ -30,6 +32,8 @@ impl Solver
         // determine which cells are in this row or column and how many squares they cover
         let mut cell_sizes_in_rows : [HashMap<u8, u8>; GRID] = Default::default();
         let mut cell_sizes_in_cols : [HashMap<u8, u8>; GRID] = Default::default();
+        // also determine in which rows and columns each cell appears
+        let mut affected_targets_by_cell = vec![Vec::new(); puzzle.num_cells() as usize];
         for row in 0..GRID
         {
             for col in 0..GRID
@@ -37,6 +41,9 @@ impl Solver
                 let cell = puzzle.sheet[row][col];
                 *cell_sizes_in_rows[row].entry(cell).or_insert(0) += 1;
                 *cell_sizes_in_cols[col].entry(cell).or_insert(0) += 1;
+                let targets = &mut affected_targets_by_cell[cell as usize];
+                targets.push(Target::Row(row));
+                targets.push(Target::Row(col));
             }
         }
 
@@ -45,6 +52,7 @@ impl Solver
             puzzle: *puzzle,
             cell_sizes_in_rows: cell_sizes_in_rows,
             cell_sizes_in_cols: cell_sizes_in_cols,
+            affected_targets_by_cell: affected_targets_by_cell,
         }
     }
 
@@ -56,7 +64,11 @@ impl Solver
     fn recurse(&self, partial: &Solution) -> Option<Solution>
     {
         let mut updated = partial.clone();
-        self.improve(&mut updated);
+        let still_possible = self.improve(&mut updated);
+        if !still_possible
+        {
+            return None;
+        }
 
         if let Some(solved) = self.puzzle.is_decided(&updated.assignment)
         {
@@ -77,36 +89,47 @@ impl Solver
         panic!("Should not get here.");
     }
 
-    fn improve(&self, partial: &mut Solution)
+    fn improve(&self, partial: &mut Solution) -> bool
     {
-        loop
+        let mut targets = HashSet::new();
+        for row_or_col in 0..GRID
         {
-            // TODO: Instead of iterating over all rows and columns like this,
-            // keep a list of rows and cols that were recently changed.
-            let mut changed = false;
-            for row_or_col in 0..GRID
-            {
-                changed |= self.improve_once(partial, Target::Row(row_or_col));
-                changed |= self.improve_once(partial, Target::Col(row_or_col));
-            }
-            if !changed { break; }
+            targets.insert(Target::Row(row_or_col));
+            targets.insert(Target::Col(row_or_col));
         }
+        while !targets.is_empty()
+        {
+            let target = targets.iter().next().cloned().unwrap();
+            targets.remove(&target);
+            let changed_cells = self.improve_once(partial, &target);
+            match changed_cells
+            {
+                Some(cells) => {
+                    for cell in cells
+                    {
+                        targets.extend(self.affected_targets_by_cell[cell as usize].clone());
+                    }        
+                },
+                None => return false
+            }
+        }
+        true
     }
 
-    fn improve_once(&self, partial: &mut Solution, row_or_col: Target) -> bool
+    fn improve_once(&self, partial: &mut Solution, row_or_col: &Target) -> Option<HashSet<u8>>
     {
         let (min, max) = self.puzzle.get_range(&partial.assignment, &row_or_col);
-        let goal = match row_or_col
+        let goal = match *row_or_col
         {
             Target::Row(r) => self.puzzle.row_counts[r],
             Target::Col(c) => self.puzzle.col_counts[c]
         };
-        if goal < min || goal > max { return false; }
+        if goal < min || goal > max { return None; }
         let mut to_fill = goal - min;
         let mut to_clear = max - goal;
 
-        let mut changed = false;
-        let cell_sizes_in_target = match row_or_col
+        let mut changed = HashSet::new();
+        let cell_sizes_in_target = match *row_or_col
         {
             Target::Row(r) => &self.cell_sizes_in_rows[r],
             Target::Col(c) => &self.cell_sizes_in_cols[c]
@@ -118,7 +141,7 @@ impl Solver
             {
                 let ref mut assign = partial.assignment[index as usize];
                 if *assign != CellState::Undecided { continue; }
-                if count > to_fill && count > to_clear { return false; }
+                if count > to_fill && count > to_clear { return None; }
                 if count > to_clear
                 {
                     *assign = CellState::Filled;
@@ -133,9 +156,9 @@ impl Solver
                 }
             }
             if seq.is_empty() { break; }
-            changed = true;
+            changed.extend(seq.clone());
             partial.sequence.extend(seq);
         }
-        changed
+        Some(changed)
     }
 }
